7ade5256690a145bd3092960e3d498ec63a50d66
luren9 <lucaseriksson007@gmail.com>
2025-12-08 18:48:20 +0100

    Fix path error in main.tf terraform file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,3 @@
-# This terraform setup should not cover retrieving data from the cluster, it should only setup a bucket which allows for storing data
-
-
 module "s3_velero_backup" {
-  source = "./modules/s3/velero"
+  source = "./modules/velero"
 }
