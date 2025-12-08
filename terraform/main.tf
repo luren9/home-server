e349b0386963fa41ec1a24673255fe035cab16e6
luren9 <lucaseriksson007@gmail.com>
2025-12-08 23:10:28 +0100

    Add outputs to main.tf root level

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,21 @@
 module "s3_velero_backup" {
   source = "./modules/velero"
 }
+
+
+output "velero_bucket_name" {
+  description = "The Velero S3 bucket name"
+  value       = module.s3_velero_backup.velero_bucket_name
+}
+
+output "velero_access_key_id" {
+  description = "Access key for Velero (like a username)"
+  value       = module.s3_velero_backup.velero_access_key_id
+  sensitive   = true
+}
+
+output "velero_secret_access_key" {
+  description = "Secret key for Velero (like a password)"
+  value       = module.s3_velero_backup.velero_secret_access_key
+  sensitive   = true
+}
